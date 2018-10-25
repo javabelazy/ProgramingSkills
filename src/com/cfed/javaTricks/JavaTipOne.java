2416b390807824f99b52cfc21f685d5f1c9b480c
Shinigami <31045202+nijeshcfed@users.noreply.github.com>
2018-10-25 10:19:51 +0530

    tip
    tip

---
+++ src/com/cfed/javaTricks/JavaTipOne.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.cfed.javaTricks;
+
+/**
+ * @author consumerfed
+ *
+ */
+public class JavaTipOne {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// \u0000d System.out.println("test");
+	}
+
+}
