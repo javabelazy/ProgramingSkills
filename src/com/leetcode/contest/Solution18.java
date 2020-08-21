5ed3259015f8576418ec09e234ff85a06999889b
KonzernTechie <kv.nijesh@gmail.com>
2020-08-21 23:05:28 +0530

    Sort Array By Parity
    Change-Id: I4b612d9f3686f78eb3f51199043b1b040f54d32f
    Signed-off-by: KonzernTechie <kv.nijesh@gmail.com>

---
+++ src/com/leetcode/contest/Solution18.java
@@ -0,0 +1,39 @@
+/**
+ * Sort Array By Parity
+ */
+package com.leetcode.contest;
+
+/**
+ * Given an array A of non-negative integers, return an array consisting of all
+ * the even elements of A, followed by all the odd elements of A.
+ * 
+ * You may return any answer array that satisfies this condition
+ * 
+ * @author cfed
+ *
+ */
+
+class Solution18 {
+
+	public int[] sortArrayByParity(int[] A) {
+
+		int even = 0;
+//		int ptr = 0;
+		for (int i = 0; i < A.length; i++) {
+			if(A[i] % 2 == 0) {
+				int temp = A[even];
+				A[even] = A[i];
+				A[i] = temp;
+				even++;
+			}
+		}
+
+		return A;
+	}
+
+	public static void main(String[] args) {
+		Solution18 s = new Solution18();
+		int[] nums = { 3, 1, 2, 4 };
+		System.out.println(s.sortArrayByParity(nums));
+	}
+}
