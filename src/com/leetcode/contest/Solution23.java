bc2967f3add9299524f8758c66e532e4a427deb9
KonzernTechie <kv.nijesh@gmail.com>
2020-10-21 23:41:04 +0530

    Asteroid Collision

---
+++ src/com/leetcode/contest/Solution23.java
@@ -0,0 +1,36 @@
+package com.leetcode.contest;
+
+/**
+ * We are given an array asteroids of integers representing asteroids in a row.
+ * 
+ * For each asteroid, the absolute value represents its size, and the sign
+ * represents its direction (positive meaning right, negative meaning left).
+ * Each asteroid moves at the same speed.
+ * 
+ * Find out the state of the asteroids after all collisions. If two asteroids
+ * meet, the smaller one will explode. If both are the same size, both will
+ * explode. Two asteroids moving in the same direction will never meet.
+ * 
+ * @author cfed
+ *
+ */
+
+class Solution23 {
+
+	public int[] asteroidCollision(int[] asteroids) {
+		
+		boolean span = true;
+		
+		int startPtr = 0;
+		
+		while(span) {
+			
+		}
+
+		return null;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
