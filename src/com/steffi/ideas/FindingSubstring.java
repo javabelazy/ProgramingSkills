d54300a9220e18a9e09a270bd5c728bd03bf8996
KonzernTechie <kv.nijesh@gmail.com>
2021-08-01 13:02:48 +0530

    To find word between two strings usign regex

--- src/com/steffi/ideas/FindingSubstring.java
+++ src/com/steffi/ideas/FindingSubstring.java
@@ -9,22 +9,25 @@ import java.util.regex.Pattern;
 /**
  * @author Apple
  * 
- * <p> To find word between two strings
+ *         <p>
+ *         To find word between two strings
+ *         
+ *         The program will get list of ID from the string using regex
+ *         </p>
+ * 
+ *         ID Pancard 
+ *         ID Aadhaar 
+ *         ID DrivingLicence
+ * 
  *
  */
 public class FindingSubstring {
-
 	public static void main(String[] args) {
-		
-		String input = "I have the following TicketType: DJs, Truck: Ford, Van: Honda";
-
-		Pattern p = Pattern.compile("(TicketType):\\s*(\\w+)");
+		String input = "I user abc is has with the following Identities ID: Pancard, ID: Aadhaar, ID: DrivingLicence";
+		Pattern p = Pattern.compile("(ID):\\s*(\\w+)");
 		Matcher m = p.matcher(input);
-
 		while (m.find()) {
-		    System.out.println(m.group(1) + "\t" + m.group(2));
+			System.out.println(m.group(1) + "\t" + m.group(2));
 		}
-		
 	}
-	
 }
